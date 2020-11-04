0abb4d67332227a99f6fb9959f512bb97d8e4864
Vikram S <vikram.senthil@outlook.com>
2020-11-04 22:23:51 +0530

    Pipeline

--- 1mg_WebScraping.R
+++ 1mg_WebScraping.R
@@ -13,3 +13,13 @@ path<-paths_allowed("https://www.1mg.com/categories/fitness-supplements/vitamins
 url<-"https://www.1mg.com/categories/fitness-supplements/vitamins-minerals-9?filter=true&sort=rating&discount=10"
 
 web<-read_html(url)
+
+#pipeline
+
+Name<-web%>%html_nodes(".style__pro-title___2QwJy")%>%html_text()
+Quantity<-web%>%html_nodes(".style__pack-size___2JQG7")%>%html_text()
+Rating<-web%>%html_nodes(".CardRatingDetail__weight-700___27w9q")%>%html_text()
+MRP<-web%>%html_nodes(".style__discount-price___25Bya")%>%html_text()
+Discount<-web%>%html_nodes(".style__off-badge___2JaF-")%>%html_text()
+SellingPrice<-web%>%html_nodes(".style__price-tag___cOxYc span")%>%html_text()
+NoOfRatings<-web%>%html_nodes(".CardRatingDetail__ratings-header___2yyQW")%>%html_text()

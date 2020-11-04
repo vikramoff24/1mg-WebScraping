3f9a905d4f7ae3c44a370371d811d1e062636320
Vikram S <vikram.senthil@outlook.com>
2020-11-04 22:18:06 +0530

    Create 1mg_WebScraping.R

---
+++ 1mg_WebScraping.R
@@ -0,0 +1,6 @@
+#-------------------------
+#Name : Vikram S
+#RegNo : RA1811003020466
+#Description : This Script contains code for scrapping a particular webpage of 1mg.
+#-------------------------
+
